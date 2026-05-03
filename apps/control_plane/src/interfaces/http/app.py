fc74e808b03a331b0a8ea5a9fd98d66f51e95060
Kane Walter <kwal0203@gmail.com>
2026-05-02 21:10:41 -0700

    Add Vercel to allowlist

--- apps/control_plane/src/interfaces/http/app.py
+++ apps/control_plane/src/interfaces/http/app.py
@@ -70,7 +70,12 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://project-lerj2.vercel.app",
+        "https://app.agentfailure.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
